5cfc88d36ef59281726341d4c0f297e205a06b18
xison chen <xison_chen@163.com>
2016-11-23 15:02:38 +0800

    Initial commit

---
+++ src/com/firstidea/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.firstidea.demo;
+
+/**
+ * Created by admin on 2016/6/20.
+ */
+public class HelloWorld {
+    public static void main(String agrs[]){
+        System.out.println("Hello World!");
+    }
+
+}
